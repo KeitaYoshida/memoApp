7bad5b84ad3fab04ecd40984d195c1e85d36f1b2
keita <ba0503naky@gmail.com>
2020-04-27 23:11:57 +0900

    0427 add BodyText

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import BodyText from './src/elements/BodyText';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//     </View>
+//   );
+// }
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hellow! world</Text>
+        <BodyText>Hi!</BodyText>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
